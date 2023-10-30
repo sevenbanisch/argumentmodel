407697b59c343e885cde5871799ea2455265ccc8
jowessels <wessels229@gmail.com>
2023-10-30 11:11:40 +0100

    added documentation in measure calculation parts of the code

--- simulation_argument_model/utilities_simulation.py
+++ simulation_argument_model/utilities_simulation.py
@@ -1,12 +1,22 @@
 import numpy as np
-from numba import jit
 
 
 def update_measure_dict(agents_eval, agents_att, interaction, measures):
+    """
+    Calculate all measures defined in measures.keys() for the given iteration.
+    The measures are added into the array given in the measures dict
+
+    :param agents_eval: evaluation of arguments for all agents numpy array. Is None in the reduced argument model
+    :param agents_att: attitudes of all agents in a numpy array
+    :param interaction: The current interation as an integer
+    :param measures: dictionary containing the measures that are to be taken
+    :return:
+        measures: the update version of the measures, that updated the arrays corresponding to the keys
+    """
 
     measures_to_be_taken = list(measures.keys())
 
-    measure_name = "attitude_all_agents"
+    measure_name = "attitude_of_all_agents"
     if measure_name in measures_to_be_taken:
         measures[measure_name][:, interaction] = agents_att.reshape((len(agents_att),))
 
@@ -20,7 +30,6 @@ def update_measure_dict(agents_eval, agents_att, interaction, measures):
 
     measure_name = "correlation_evaluations"
     if measure_name in measures_to_be_taken:
-        measure_name["hallo"] = 1
         raise Exception("Not yet implemented :)")
 
     return measures
